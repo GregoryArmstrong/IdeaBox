11cd8c9c22ea263656f2a96a580e49e259a21708
GregoryArmstrong <intergalacticgentleman@gmail.com>
2016-03-21 15:35:13 -0600

    Add javascript to make ajax call for ideas/display them on index
    Added javascript functions which wait for the root page to load, and then make an AJAX
    call to the Ideas API endpoint. Once the ideas are obtained, it creates <li> elements
    for each idea, and appends them to the page for display.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get '/', to: "home#index", as: :root
+
   namespace :api do
     namespace :v1, defaults: { format: :json } do
       resources :ideas, only: [:index]
