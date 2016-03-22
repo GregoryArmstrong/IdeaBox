930d77f21fa561b7bb4d5fd8e2c1d7bb6695903e
GregoryArmstrong <intergalacticgentleman@gmail.com>
2016-03-22 12:59:28 -0600

    Add function to delete an idea from the list
    Added function to delete an idea from the list which is triggered by clicking
    the delete link associated with each line item.

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -14,4 +14,12 @@ class Api::V1::IdeasController < Api::ApiController
     respond_with(@idea, :status => :created, :location => api_v1_ideas_path)
   end
 
+  def destroy
+    @idea = Idea.find(params[:id])
+    @idea.destroy
+    respond_with @idea do |format|
+      format.js { head :ok }
+    end
+  end
+
 end
