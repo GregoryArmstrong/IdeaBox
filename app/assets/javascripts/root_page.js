a641e1550a3da6ec946bfccc860cf6f146d141a9
GregoryArmstrong <intergalacticgentleman@gmail.com>
2016-03-21 19:46:40 -0600

    Add create idea form to root page
    Added a create idea form to the root page, which POSTs to the idea controller to
    create an idea with the form's field data, updates the DOM and clears the form's field data

--- app/assets/javascripts/root_page.js
+++ app/assets/javascripts/root_page.js
@@ -1,6 +1,38 @@
 $(document).ready(function(){
   queryAllIdeas();
+  $('#submit_button').on('click', submitNewIdea)
 })
+
+function submitNewIdea(){
+  $.ajax({
+    url: "api/v1/ideas",
+    type: "POST",
+    data: { title: ideaTitle(),
+            body: ideaBody()
+          },
+    success: function(response){
+      console.log('success', response);
+      createListElement(response);
+      clearForm();
+    },
+    error: function(xhr){
+      console.log('fail', xhr);
+    }
+  })
+};
+
+function clearForm(){
+  $('#idea_title').val("")
+  $('#idea_body').val("")
+}
+
+function ideaTitle(){
+  return $('#idea_title').val()
+}
+
+function ideaBody(){
+  return $('#idea_body').val()
+}
 
 function queryAllIdeas(){
   $.ajax({
@@ -21,4 +53,8 @@ function createListElements(ideas){
   ideas.forEach(function(idea){
       $("#list").append( $("<li>" + "Title: " + idea.title + " Body: " + idea.body + " Quality: " + idea.quality + "</li>"));
   })
+}
+
+function createListElement(idea){
+  $("#list").append( $("<li>" + "Title: " + idea.title + " Body: " + idea.body + " Quality: " + idea.quality + "</li>"));
 }
