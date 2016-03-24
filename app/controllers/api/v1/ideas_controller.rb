8b6b91cebfd759495bd0c219fb50758c34470b68
GregoryArmstrong <intergalacticgentleman@gmail.com>
2016-03-24 10:23:45 -0600

    Changes necessary for heroku deployment

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::IdeasController < Api::ApiController
   respond_to :json
 
   def index
-    respond_with Idea.all
+    respond_with Idea.all.order(created_at: :desc)
   end
 
   def create
