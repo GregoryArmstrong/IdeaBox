a526615e832d6c34cb372a64eb1287b07b61eb29
GregoryArmstrong <intergalacticgentleman@gmail.com>
2016-03-24 10:54:25 -0600

    final heroku changes

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::IdeasController < Api::ApiController
   respond_to :json
 
   def index
-    respond_with Idea.all.order(created_at: :desc)
+    respond_with Idea.all.order(created_at: :asc)
   end
 
   def create
